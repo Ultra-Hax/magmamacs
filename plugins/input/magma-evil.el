2d670a4a79b72a6dd66a08a16e8f58518b4efca3
Ultra-Hax <68920205+Ultra-Hax@users.noreply.github.com>
2021-04-30 10:05:01 -0400

    Added redo support for evil mode

--- plugins/input/magma-evil.el
+++ plugins/input/magma-evil.el
@@ -1,14 +1,21 @@
 ;;; magma-evil.el -*- lexical-binding: t -*-
 
 (use-package evil 
-  	:init
-  	(setq evil-want-keybinding nil)	
-	:config (evil-mode))
+  :defer t
+  :init
+  (setq evil-want-keybinding nil)	
+  :hook (after-init . evil-mode))
 
 (use-package evil-collection
 	:after evil
-  	:defer 1 
+  	:defer t 
   	:config
   	(evil-collection-init))
+(use-package undo-tree
+  :after evil
+  :diminish
+  :config
+  (evil-set-undo-system 'undo-tree)
+  (global-undo-tree-mode 1))
 
 (provide 'magma-evil)
